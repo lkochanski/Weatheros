489b08aed20b454d37c9f0f8e49b682d593033b7
Natharel <kochanski90@gmail.com>
2020-05-03 18:24:11 +0200

    Add files via upload

---
+++ precache-manifest.29ec462a0c856c60ffc7759e20db99eb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eacb28c27e5fbeb34f898e4a26459819",
+    "url": "/weatheros/index.html"
+  },
+  {
+    "revision": "8a774ecf4709e5ea8764",
+    "url": "/weatheros/static/css/2.b706bf63.chunk.css"
+  },
+  {
+    "revision": "85ac362f0a307e467772",
+    "url": "/weatheros/static/css/main.745370dc.chunk.css"
+  },
+  {
+    "revision": "8a774ecf4709e5ea8764",
+    "url": "/weatheros/static/js/2.c7d3db3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatheros/static/js/2.c7d3db3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85ac362f0a307e467772",
+    "url": "/weatheros/static/js/main.a9ff8db1.chunk.js"
+  },
+  {
+    "revision": "1ea767bb84d9961f0e4a",
+    "url": "/weatheros/static/js/runtime-main.9340d968.js"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weatheros/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weatheros/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weatheros/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weatheros/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/weatheros/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
